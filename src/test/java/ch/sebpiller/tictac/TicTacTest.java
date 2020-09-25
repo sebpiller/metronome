ddb4fc250638f37bbe55ecd24295d70bb7007fa4
Séb Piller <me@sebpiller.ch>
2020-09-25 20:34:01 +0200

    .

--- src/test/java/ch/sebpiller/tictac/TicTacTest.java
+++ src/test/java/ch/sebpiller/tictac/TicTacTest.java
@@ -16,14 +16,7 @@ public class TicTacTest {
 
     // the test is run this length minimum
     static final int TEST_MIN_TICKS_TO_VALIDATE = 50;
-    private final TicTac.TicTacListener slowWatcher = (ticOrTac, expectedTempo) -> {
-        watcher.beat(ticOrTac, expectedTempo);
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            // ignore
-        }
-    };
+
     @Parameterized.Parameter
     public int bpm = 0;
     // just returns bpm
@@ -68,6 +61,14 @@ public class TicTacTest {
         }
 
         last = now;
+    };
+    private final TicTac.TicTacListener slowWatcher = (ticOrTac, expectedTempo) -> {
+        watcher.beat(ticOrTac, expectedTempo);
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            // ignore
+        }
     };
     private TicTacBuilder builder = new TicTacBuilder().withListener(watcher);
 
